289c14a4dd2a5ea7c496fb570a799fa5e8e35926
pigu <a8568730@gmail.com>
2018-11-20 11:51:17 +0800

    批次錄音稿.sh 共檔名提出來

--- 批次錄音稿odt轉csv.sh
+++ 批次錄音稿odt轉csv.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 find ../ -type f -name '*.odt' | while read inputName ; do
-  outputName=${inputName%.odt}
+  #echo $inputName
+  outputName=`basename "$inputName"` 
   echo $outputName
   #python 錄音稿的odt轉csv.py $inputName
 done
