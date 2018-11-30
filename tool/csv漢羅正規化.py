54de79ebd5cfd292a46c37109ab7ce83e5cb6e9a
a8568730 <a8568730@hotmail.com>
2018-11-30 17:54:30 +0800

    .travis.yml改用xargs

--- tool/csv漢羅正規化.py
+++ tool/csv漢羅正規化.py
@@ -1,25 +1,33 @@
 import sys
 import csv
-from .csv漢字正規化 import 漢字正規化
+from tool.正規化漢字 import 漢字正規化
 
 
 def csv正規化(輸入檔名):
+    kiatKo = []
     with open(輸入檔名, 'r') as csv檔:
         csv檔頭 = csv.reader(csv檔)
-        tshingTuann = list(csv檔頭)
-        print(tshingTuann)
-        return 
-        kiatKo = []
-        for tsua in csv檔頭:
-            temp = tsua
-            hanji = 漢字正規化(temp['Hàn-jī'])
-            kiatKo.append(temp.update({
-                hanji
+        bunTsiunn = list(csv檔頭)
+        print('bunTsiunn=', bunTsiunn)
+        for tsua in bunTsiunn:
+            piHun = dict(tsua)
+            print('pihun={}', piHun)
+            tsingKuiHanji = 漢字正規化(piHun['Hàn-jī'])
+            kiatKo.append(piHun.update({
+                'Tsing-kui-hàn-jī': tsingKuiHanji,
             }))
 
-    with open(輸入檔名, 'w') as 正規化檔:
-        正規化檔頭 = csv.DictWriter(正規化檔)
+    with open(輸入檔名, 'w') as csv檔:
+        try:
+            fieldnames = bunTsiunn[0].keys()
+        except Exception as e:
+            raise
+        csv檔頭 = csv.DictWriter(csv檔, fieldnames=fieldnames)
+        csv檔頭.writeheader()
+        for tsua in kiatKo:  
+            csv檔頭.writerow(tsua)
             
+    csv檔頭.writerow({'first_name': 'Baked', 'last_name': 'Beans'}) 
             
 if __name__ == '__main__':
     輸入檔名 = sys.argv[1]
