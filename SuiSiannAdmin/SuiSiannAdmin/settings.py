6be6ea648349447bfc414dbec1f8afd3f710bd99
a8568730 <a8568730@hotmail.com>
2018-12-04 18:23:47 +0800

    改轉去MEDIA_ROOT=wavs/

--- SuiSiannAdmin/SuiSiannAdmin/settings.py
+++ SuiSiannAdmin/SuiSiannAdmin/settings.py
@@ -119,5 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'wavs')
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'wavs')
+MEDIA_URL = '/wavs/'
 
--- SuiSiannAdmin/SuiSiannAdmin/urls.py
+++ SuiSiannAdmin/SuiSiannAdmin/urls.py
@@ -21,4 +21,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] + static('wavs', document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
