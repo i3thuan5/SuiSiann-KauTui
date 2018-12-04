f2df9fd3f53475e750eed309a4bb8f44ab3481af
a8568730 <a8568730@hotmail.com>
2018-12-04 16:43:23 +0800

    修改對齊狀態的引數

--- SuiSiannAdmin/sunko/management/commands/匯入csv.py
+++ SuiSiannAdmin/sunko/management/commands/匯入csv.py
@@ -4,6 +4,7 @@ from sunko.models import 句表, 文章表
 from os.path import basename
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from 臺灣言語工具.解析整理.解析錯誤 import 解析錯誤
+import csv
 
 
 class Command(BaseCommand):
@@ -19,13 +20,15 @@ class Command(BaseCommand):
             一文章 = 文章表(文章名=tongMia)
             一文章.save()
             # Khiam 文章的句
-            for tsua in csvPath:
+            csvDict = csv.DictReader(csvPath)
+            for tsua in csvDict:
+                print(tsua['Im-tóng'], type(tsua['Im-tóng']))
                 一句 = 句表(
                     來源=一文章,
                     音檔=tsua['Im-tóng'],
                     漢字=tsua['Hàn-jī'],
                     臺羅=tsua['Lô-má-jī'],
-                    對齊狀態=檢查對齊狀態(tsua),
+                    對齊狀態=檢查對齊狀態(tsua['Hàn-jī'], tsua['Lô-má-jī'])
                 )
                 一句.save()
 
@@ -36,9 +39,9 @@ def 提著csv檔名(csv路徑):
     純檔名 = 檔名[:點所在].replace('_hanlo', '')
     return 純檔名
 
-def 檢查對齊狀態(tsua):
+def 檢查對齊狀態(hanji, romaji):
     try:
-        拆文分析器.對齊句物件(tsua['Hàn-jī'], tsua['Lô-má-jī'])
+        拆文分析器.對齊句物件(hanji, romaji)
     except 解析錯誤 as 錯誤:
         return str(錯誤)
     else:
