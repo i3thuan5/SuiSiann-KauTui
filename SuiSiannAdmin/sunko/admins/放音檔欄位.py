af9510729184cd7771d0cfd347b8bc34d9f5aa7b
a8568730 <a8568730@hotmail.com>
2018-12-05 16:54:57 +0800

    *修好音檔網址 *記音檔所在.py 改名做 算音檔網址.py

--- SuiSiannAdmin/sunko/admins/放音檔欄位.py
+++ SuiSiannAdmin/sunko/admins/放音檔欄位.py
@@ -1,7 +1,5 @@
 from django.utils.html import format_html
-from sunko.management.記音檔所在 import 音檔所在表
-from os.path import isfile
-from urllib.parse import urlencode
+from sunko.management.記音檔所在 import 音檔網址表
 
 
 class 放音檔欄位():
@@ -9,13 +7,13 @@ class 放音檔欄位():
     def 放音檔(self, obj):
         if obj.音檔:
             try:
-                音檔完整所在 = 音檔所在表[obj.音檔]
+                音檔網址 = 音檔網址表[obj.音檔]
             except KeyError as e:
                 return e
             return format_html('''<audio controls>
                 <source src='{}'>
                 Your browser does not support the audio element.</audio>''',
-                    音檔完整所在
+                    音檔網址
                 )
         else:
             return ''
