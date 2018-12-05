b41dffd84f3661741208032f94f60f2ab21570a1
a8568730 <a8568730@hotmail.com>
2018-12-05 18:19:40 +0800

    +句表單.py 指定編輯欄位的長短

--- SuiSiannAdmin/sunko/admins/句後台.py
+++ SuiSiannAdmin/sunko/admins/句後台.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from sunko.admins.對齊thai仔 import 對齊thai仔
 from django.utils.html import format_html
 from sunko.admins.放音檔欄位 import 放音檔欄位
-from django import forms
+from sunko.admins.句表單 import 句表單
 
 
 
@@ -15,4 +15,4 @@ class 句後台(admin.ModelAdmin, 放音檔欄位):
     # change view
     readonly_fields = ('音檔','放音檔', '修改時間', '對齊狀態')
     fields = ('漢字', '臺羅', '音檔','放音檔', '對齊狀態', '修改時間', )
-    
+    form = 句表單
