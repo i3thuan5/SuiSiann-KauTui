9d6330daf3386b0ffdc272eaae6fc7e4ec5271a6
a8568730 <a8568730@hotmail.com>
2018-12-05 16:25:27 +0800

    +記音檔所在表

--- SuiSiannAdmin/sunko/admins/放音檔欄位.py
+++ SuiSiannAdmin/sunko/admins/放音檔欄位.py
@@ -1,14 +1,21 @@
 from django.utils.html import format_html
+from sunko.management.記音檔所在 import 音檔所在表
+from os.path import isfile
+from urllib.parse import urlencode
 
 
 class 放音檔欄位():
 
     def 放音檔(self, obj):
         if obj.音檔:
+            try:
+                音檔完整所在 = 音檔所在表[obj.音檔]
+            except KeyError as e:
+                return e
             return format_html('''<audio controls>
-            <source src='/wavs/{}'>
-            Your browser does not support the audio element.</audio>''',
-                obj.音檔
-            )
+                <source src='{}'>
+                Your browser does not support the audio element.</audio>''',
+                    音檔完整所在
+                )
         else:
             return ''
