170efa29669f4a533b405ea3df2c6aa501bb8f26
a8568730 <a8568730@hotmail.com>
2018-12-11 11:25:37 +0800

    * 共檢查對齊kah檢查用字寫佇kang一个函式

--- SuiSiannAdminApp/management/檢查對齊狀態.py
+++ SuiSiannAdminApp/management/檢查對齊狀態.py
@@ -1,23 +1,23 @@
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from 臺灣言語工具.解析整理.解析錯誤 import 解析錯誤
+from 用字.models import 用字表
 
 
 def 檢查對齊狀態(hanji, lomaji):
-    對齊 = 檢查對齊(hanji, lomaji)
-    if 對齊 is not True:
-        return 對齊
-    
-    用字 = 檢查對齊()
-
-
-def 檢查對齊(hanji, lomaji):
     try:
-        拆文分析器.對齊句物件(hanji, lomaji)
+        句物件 = 拆文分析器.對齊句物件(hanji, lomaji)
     except 解析錯誤 as 錯誤:
         return str(錯誤)
-    else:
-        return True
-
-
-def 檢查用字(hanji, lomaji):
     
+    毋著的字 = []
+    字物陣列 = 句物件.篩出字物件()
+    for 一字物 in 字物陣列:
+        敢有 = 用字表.有這个字無(一字物)
+        if 敢有 is not True and 一字物.型 != 一字物.音:
+            毋著的字.append(一字物)
+    
+    if 毋著的字:
+        return str(毋著的字)
+    else:
+        return ''
+            
