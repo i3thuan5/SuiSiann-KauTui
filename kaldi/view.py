43f8e25921871a04f51d44f99bdeeb9432b89eb4
薛丞宏 <ihcaoe@gmail.com>
2019-01-02 15:53:01 +0800

    flake8

--- kaldi/view.py
+++ kaldi/view.py
@@ -1,29 +1,28 @@
 from urllib.parse import quote
 import http
 import json
-from sys import stderr
 from django.http.response import JsonResponse
+
+
 def kiamtsa(request):
- conn = http.client.HTTPConnection("it",port=5000)
- han='坐 咧'
- tong='Nov 3, 2018/動詞/Nov 3, 2018.wav'
- conn.request("GET", quote("/{}/{}".format(han,tong)))
- r1 = conn.getresponse()
- print(r1.status, r1.reason)
- a=r1.read()
- print(a)
- data1 = json.loads(a)  # This will return entire content.
- print(data1,'data1')
- print(data1,'data1',file=stderr)
- conn = http.client.HTTPConnection("ji",port=5000)
- conn.request("GET", data1)
- r1 = conn.getresponse()
- print(r1.status, r1.reason)
- data2 = r1.read()  # This will return entire content.
- conn = http.client.HTTPConnection("sann",port=5000)
- conn.request("GET", data1)
- r3 = conn.getresponse()
- print(r3.status, r3.reason)
- data3 = json.loads(r3.read())  # This will return entire content.
- print(data3)
- return JsonResponse({'LMJ': data3})
+    conn = http.client.HTTPConnection("it", port=5000)
+    han = '坐 咧'
+    tong = 'Nov 3, 2018/動詞/Nov 3, 2018.wav'
+    conn.request("GET", quote("/{}/{}".format(han, tong)))
+    r1 = conn.getresponse()
+    print(r1.status, r1.reason)
+    a = r1.read()
+    print(a)
+    data1 = json.loads(a)  # This will return entire content.
+    conn = http.client.HTTPConnection("ji", port=5000)
+    conn.request("GET", data1)
+    r1 = conn.getresponse()
+    print(r1.status, r1.reason)
+    r1.read()  # This will return entire content.
+    conn = http.client.HTTPConnection("sann", port=5000)
+    conn.request("GET", data1)
+    r3 = conn.getresponse()
+    print(r3.status, r3.reason)
+    data3 = json.loads(r3.read())  # This will return entire content.
+    print(data3)
+    return JsonResponse({'LMJ': data3})
