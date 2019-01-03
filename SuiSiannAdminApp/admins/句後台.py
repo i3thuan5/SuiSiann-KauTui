d22a089a86f801e1d4aded39875eabd94c6cd0b0
a8568730 <a8568730@hotmail.com>
2019-01-03 10:14:02 +0800

    * 句後台.py，揤save了後更新修改時間。

--- SuiSiannAdminApp/admins/句後台.py
+++ SuiSiannAdminApp/admins/句後台.py
@@ -3,10 +3,11 @@ from SuiSiannAdminApp.admins.對齊thai仔 import 對齊thai仔
 from SuiSiannAdminApp.admins.放音檔欄位 import 放音檔欄位
 from SuiSiannAdminApp.admins.句表單 import 句表單
 from SuiSiannAdminApp.admins.action正規化漢字 import 漢字括號共提掉
+from django.utils.timezone import now
 
 
 class 句後台(admin.ModelAdmin, 放音檔欄位):
-    list_display = ['id', '放音檔', '漢字', '臺羅', '對齊狀態']
+    list_display = ['id', '放音檔', '漢字', '臺羅', '對齊狀態', '修改時間']
     list_filter = ['來源', 對齊thai仔]
     ordering = ['id']
     list_per_page = 10
@@ -16,3 +17,7 @@ class 句後台(admin.ModelAdmin, 放音檔欄位):
     readonly_fields = ('音檔', '放音檔', '修改時間', '對齊狀態')
     fields = ('漢字', '臺羅', '對齊狀態', '放音檔', '音檔', '修改時間', )
     form = 句表單
+
+    def save_model(self, request, obj, form, change):
+        obj.修改時間 = now()
+        super(句後台, self).save_model(request, obj, form, change)
