9df33865fb29fc4d230d7d2b65f6c9137b947f71
a8568730 <a8568730@hotmail.com>
2019-01-21 22:41:45 +0800

    + commands/更新對齊狀態.py

---
+++ SuiSiannAdminApp/management/commands/更新對齊狀態.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from SuiSiannAdminApp.models import 句表
+from django.db.utils import IntegrityError
+
+
+
+class Command(BaseCommand):
+    help='(oo)重算資料庫內底逐句的對齊狀態'
+
+    def handle(self, *args, **options):
+        for 句 in 句表.objects.all():
+            # 儉句會重算對齊狀態
+            句.save()
+
+
