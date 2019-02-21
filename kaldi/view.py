07b7ec746a292281527e919bfc55ce9e66af5af2
Fafofafoy <ihcaoe@gmail.com>
2019-02-21 11:50:44 +0800

    Siu view

--- kaldi/view.py
+++ kaldi/view.py
@@ -11,29 +11,38 @@ from 臺灣言語工具.基本物件.公用變數 import 標點符號
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 
 
+'exp/chain/tdnn_1a_sp/frame_subsampling_factor'
+frame_subsampling_factor = 3
+
+
 def kiamtsa(request, kuid):
     ku = 句表.objects.get(id=kuid)
-    han = []
-    for ji in 拆文分析器.建立句物件(ku.漢字).篩出字物件():
-        if ji not in 標點符號:
-            han.append(ji.型)
-    tong = 音檔網址表[ku.音檔][6:]
-    conn = http.client.HTTPConnection("it", port=5000)
-    conn.request("GET", quote("/{}/{}".format(' '.join(han), tong)))
-    r1 = conn.getresponse()
-    print(r1.status, r1.reason)
-    a = r1.read()
-    print(a)
-    data1 = json.loads(a)  # This will return entire content.
-    conn = http.client.HTTPConnection("ji", port=5000)
-    conn.request("GET", data1)
-    r1 = conn.getresponse()
-    print(r1.status, r1.reason)
-    r1.read()  # This will return entire content.
-    conn = http.client.HTTPConnection("sann", port=5000)
-    conn.request("GET", data1)
-    r3 = conn.getresponse()
-    print(r3.status, r3.reason)
-    data3 = json.loads(r3.read())  # This will return entire content.
-    print(data3)
-    return JsonResponse({'LMJ': data3})
+    kiatko = tuìtsê(音檔網址表[ku.音檔][6:], ku.羅馬字.split('\n'))
+    return JsonResponse({'LMJ': kiatko})
+
+
+def tuìtsê(wavPath, taiBun):
+    網址 = "/?taibun={}&wav={}".format(
+        quote(json.dumps(taiBun)),
+        quote(wavPath),
+    )
+
+    it_conn = http.client.HTTPConnection('it', port=5000)
+    it_conn.request("GET", 網址)
+    tmpPath = json.loads(it_conn.getresponse().read())
+    print('it tmpPath', tmpPath)
+    ji_conn = http.client.HTTPConnection('ji', port=5000)
+    ji_conn.request("GET", tmpPath)
+    print('ji',  json.loads(ji_conn.getresponse().read()))
+    sam_conn = http.client.HTTPConnection("sam", port=5000)
+    sam_conn.request("GET", tmpPath)
+    sikan = json.loads(sam_conn.getresponse().read())
+    print('sam', sikan)
+    kiatko = []
+    for tsua in sikan:
+        ku = tsua.split()
+        ku[2] = str(float(ku[2]) * frame_subsampling_factor)
+        ku[3] = str(float(ku[3]) * frame_subsampling_factor)
+        kiatko.append(ku)
+        print(' '.join(ku))
+    return kiatko
