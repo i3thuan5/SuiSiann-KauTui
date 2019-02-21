25291764570046a22e9608a01048ddc449f84390
a8568730 <a8568730@hotmail.com>
2019-02-21 17:50:34 +0800

    * 換算切音所在.py，頂句尾=頂句頭+區間

--- kaldi/lib/換算切音所在.py
+++ kaldi/lib/換算切音所在.py
@@ -9,8 +9,8 @@ def 換算切音所在(總時間, kaldi結果):
     換算結果 = []
     頂一中央 = 0.0 
     for ind in range(1, kaldi結果數):
-        # [head0, tail0], [head1, tail1], ...
-        頂句尾 = float(kaldi結果[ind - 1][3])
+        # [head0, duration0], [head1, duration1], ...
+        頂句尾 = float(kaldi結果[ind - 1][2]) + float(kaldi結果[ind - 1][3])
         句頭 = float(kaldi結果[ind][2])
         中央 = (句頭 + 頂句尾) / 2
         換算結果.append([頂一中央, 中央,])
