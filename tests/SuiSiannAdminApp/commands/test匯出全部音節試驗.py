730f04da15e3b5ff348702e4b7efb6c218937497
fafoy <ihcaoe@gmail.com>
2019-02-25 15:11:50 +0800

    Ka tsuanpoo imtsiat tshigiam

---
+++ tests/SuiSiannAdminApp/commands/test匯出全部音節試驗.py
@@ -0,0 +1,34 @@
+import json
+from os.path import join
+from tempfile import TemporaryDirectory
+
+from SuiSiannAdminApp.models import 句表, 文章表
+from django.core.management import call_command
+from django.test.testcases import TestCase
+
+
+class 匯出全部音節試驗(TestCase):
+    def test_無聲調(self):
+        self.hue('sui2 koo-niû')
+        self.assertEqual(self.theh()['無聲調'], {'sui', 'koo', 'niu'})
+
+    def test_有聲調(self):
+        self.hue('sui2 koo-niû')
+        self.assertEqual(self.theh()['有聲調'], {'sui2', 'koo1', 'niu5'})
+
+    def hue(self, ji):
+        句表.objects.create(
+            來源=文章表.objects.create(文章名='33'),
+            音檔=None,
+            原始漢字=ji,
+            原始臺羅=ji,
+            漢字=ji,
+            臺羅=ji,
+        )
+
+    def theh(self):
+        with TemporaryDirectory() as sootsai:
+            tongmia = join(sootsai, 'lmj.json')
+            call_command('匯出全部音節', tongmia)
+            with open(tongmia) as tong:
+                return json.load(tong)
