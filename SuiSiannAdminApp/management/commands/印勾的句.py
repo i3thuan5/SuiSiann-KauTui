ae684c4b3593a5b2b2b6b8d0f63e4d0409f74b85
a8568730 <a8568730@hotmail.com>
2019-03-08 18:27:05 +0800

    * reformat

--- SuiSiannAdminApp/management/commands/印勾的句.py
+++ SuiSiannAdminApp/management/commands/印勾的句.py
@@ -1,11 +1,5 @@
-import json
-
 from SuiSiannAdminApp.models import 句表
 from django.core.management.base import BaseCommand
-
-
-from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
-from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
 
 
 class Command(BaseCommand):
