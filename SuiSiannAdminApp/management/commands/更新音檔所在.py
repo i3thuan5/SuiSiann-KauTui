8678e6488c425073fa375105ba4d2b1be6aeafba
a8568730 <a8568730@hotmail.com>
2019-04-01 17:09:29 +0800

    * reformat

--- SuiSiannAdminApp/management/commands/更新音檔所在.py
+++ SuiSiannAdminApp/management/commands/更新音檔所在.py
@@ -1,7 +1,5 @@
 from django.core.management.base import BaseCommand
 from SuiSiannAdminApp.models import 句表
-from django.db.models import F
-from django.db.models.expressions import Func, Value
 import csv
 
 
@@ -10,7 +8,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('csv', type=str, help='欲改ê句')
-        
+
     def handle(self, *args, **options):
         with open(options['csv'], 'r') as csvTong:
             reader = csv.DictReader(csvTong)
@@ -18,7 +16,7 @@ class Command(BaseCommand):
                 更新音檔所在(tsua['句編號'], tsua['頭四字'], tsua['新音檔所在'])
 
 
-def 更新音檔所在(id, 漢字, 新檔):
-    這句 = 句表.objects.get(id=id, 漢字__startswith=漢字)
-    這句.音檔=新檔
+def 更新音檔所在(ID, 漢字, 新檔):
+    這句 = 句表.objects.get(id=ID, 漢字__startswith=漢字)
+    這句.音檔 = 新檔
     這句.save()
