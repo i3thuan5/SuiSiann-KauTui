6a974778a5884914bed5a59984f78fd48ed9b738
a8568730 <a8568730@hotmail.com>
2019-04-01 16:28:36 +0800

    + commands/更新音檔所在

--- SuiSiannAdminApp/management/commands/更新音檔所在.py
+++ SuiSiannAdminApp/management/commands/更新音檔所在.py
@@ -7,14 +7,16 @@ from django.db.models.expressions import Func, Value
 class Command(BaseCommand):
     help = '更新資料庫內底的音檔所在'
 
+    def add_arguments(self, parser):
+        parser.add_argument('id', type=str, help='句表的ID')
+        parser.add_argument('hanji', type=str, help='漢字')
+        parser.add_argument('sinTong', type=str, help='新的音檔所在')
+
     def handle(self, *args, **options):
-        更新音檔所在()
+        更新音檔所在(options['id'], options['hanji'], options['sinTong'])
 
 
-def 更新音檔所在():
-    句表.objects.filter(音檔__contains='Oct 8,').update(
-        音檔=Func(
-            F('音檔'),
-            Value('Oct 8,'), Value('Oct 13,'),
-            function='replace'
-        ))
+def 更新音檔所在(id, 漢字, 新檔):
+    這句 = 句表.objects.get(id=id, 漢字__startswith=漢字)
+    這句.音檔=新檔
+    這句.save()
