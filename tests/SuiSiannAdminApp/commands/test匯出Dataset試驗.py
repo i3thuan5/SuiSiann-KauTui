39700c501cb3e7a094ca6069d9358dde3301a098
fafoy <ihcaoe@gmail.com>
2019-04-04 10:04:39 +0800

    補試驗

--- tests/SuiSiannAdminApp/commands/test匯出Dataset試驗.py
+++ tests/SuiSiannAdminApp/commands/test匯出Dataset試驗.py
@@ -1,6 +1,9 @@
+from csv import DictReader
+from filecmp import cmp
 import json
 from os.path import join
 from tempfile import TemporaryDirectory
+import wave
 
 from SuiSiannAdminApp.models import 句表, 文章表
 from django.core.management import call_command
@@ -10,12 +13,32 @@ from django.test.testcases import TestCase
 class 匯出Dataset試驗(TestCase):
     def test_資料照來源排(self):
         with TemporaryDirectory() as tsuliaugiap:
-            it = join(tsuliaugiap, '1.wav')
+            im = join(tsuliaugiap, 'Oct 3.wav')
+            self.siaimtong(im)
             文章 = 文章表.objects.create(文章名='33')
-        self.fail()
+            self.hue(文章, 'sui', im)
+
+            kiatko = join(tsuliaugiap, 'kiatko')
+            call_command('匯出Dataset', kiatko)
+            with open(join(kiatko, 'sui-siann.csv')) as tong:
+                tsitpit = list(DictReader(tong))[0]
+                self.assertEqual(tsitpit['音檔'], 'ImTong/SuiSiann_0001.wav')
+                self.assertEqual(tsitpit['來源'], '33')
+                self.assertEqual(tsitpit['漢字'], 'sui')
+                self.assertEqual(tsitpit['羅馬字'], 'sui')
 
     def test_音檔名重編號(self):
-        self.fail()
+        with TemporaryDirectory() as tsuliaugiap:
+            im = join(tsuliaugiap, 'Oct 3.wav')
+            self.siaimtong(im)
+            文章 = 文章表.objects.create(文章名='33')
+            self.hue(文章, 'sui', im)
+
+            kiatko = join(tsuliaugiap, 'kiatko')
+            call_command('匯出Dataset', kiatko)
+            self.assertTrue(
+                cmp(im), join(tsuliaugiap, 'ImTong/SuiSiann_0001.wav')
+            )
 
     def hue(self, 文章, ji, imtong):
         句表.objects.create(
