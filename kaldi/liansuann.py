8ecfadc7554de35ea8240ba58cbd3d1de838944d
a8568730 <a8568730@hotmail.com>
2019-12-01 18:42:34 +0800

    Tuitse 1 ai post

--- kaldi/liansuann.py
+++ kaldi/liansuann.py
@@ -1,6 +1,7 @@
 import http
+from http.client import HTTPConnection
 import json
-from urllib.parse import quote
+from urllib.parse import quote, urlencode
 
 
 'exp/chain/tdnn_1a_sp/frame_subsampling_factor'
@@ -8,19 +9,22 @@ frame_subsampling_factor = 3
 
 
 def tuìtsê(wavPath, taiBun):
-    網址 = "/?taibun={}&wav={}".format(
-        quote(json.dumps(taiBun)),
-        quote(wavPath),
-    )
-
-    it_conn = http.client.HTTPConnection('it', port=5000)
-    it_conn.request("GET", 網址)
+    參數 = urlencode({
+        'taibun': json.dumps(taiBun),
+        'wav': (wavPath),
+    })
+    headers = {
+        "Content-type": "application/x-www-form-urlencoded",
+        "Accept": "text/plain"
+    }
+    it_conn = HTTPConnection('tuitse-it', port=5000)
+    it_conn.request("POST", '/', 參數, headers)
     tmpPath = json.loads(it_conn.getresponse().read())
     print('it tmpPath', tmpPath)
-    ji_conn = http.client.HTTPConnection('ji', port=5000)
+    ji_conn = HTTPConnection('ji', port=5000)
     ji_conn.request("GET", tmpPath)
     print('ji',  json.loads(ji_conn.getresponse().read()))
-    sam_conn = http.client.HTTPConnection("sam", port=5000)
+    sam_conn = HTTPConnection("sam", port=5000)
     sam_conn.request("GET", tmpPath)
     sikan = json.loads(sam_conn.getresponse().read())
     print('sam', sikan)
