723108adde4a8dd39bccb8ee4ec986c1471c1e30
Fafofafoy <ihcaoe@gmail.com>
2021-03-12 11:49:01 +0800

    Ka nginx ê dockerfile.

---
+++ SuiSiannAdmin/docker_tsuki.py
@@ -0,0 +1,16 @@
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+STATIC_ROOT = '/static/'
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'ithuan',
+#         'HOST': 'postgres',
+#         'PORT': '',
+#     }
+# }
