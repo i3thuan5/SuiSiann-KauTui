7fbfa57b5fd246b2a3c38fbcd80fbb37ff735e7e
Fafofafoy <ihcaoe@gmail.com>
2021-06-25 14:50:59 +0800

    Ka postgres

--- SuiSiannAdmin/docker_tsuki.py
+++ SuiSiannAdmin/docker_tsuki.py
@@ -4,13 +4,13 @@ DEBUG = False
 
 STATIC_ROOT = '/static/'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'ithuan',
-#         'HOST': 'postgres',
-#         'PORT': '',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'ithuan',
+        'HOST': 'postgres',
+        'PORT': '',
+    }
+}
