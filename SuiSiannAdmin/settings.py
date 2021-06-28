205c6e45d00370d8acb98ee978a419ed81afac7a
Fafofafoy <ihcaoe@gmail.com>
2021-06-28 11:50:59 +0800

    template khioh-khioh--leh

--- SuiSiannAdmin/settings.py
+++ SuiSiannAdmin/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'SuiSiannAdmin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
