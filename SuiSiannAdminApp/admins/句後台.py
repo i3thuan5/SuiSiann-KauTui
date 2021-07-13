0244b7e0ef3e35835716d6566f3578420a291180
Fafofafoy <ihcaoe@gmail.com>
2021-07-13 12:08:37 +0800

    flake8

--- SuiSiannAdminApp/admins/句後台.py
+++ SuiSiannAdminApp/admins/句後台.py
@@ -5,7 +5,6 @@ from SuiSiannAdminApp.admins.對齊thai仔 import 對齊thai仔
 from SuiSiannAdminApp.admins.放音檔欄位 import 放音檔欄位
 from SuiSiannAdminApp.admins.句表單 import 句表單
 from SuiSiannAdminApp.admins.action正規化漢字 import 漢字括號共提掉
-from SuiSiannAdminApp.admins.action重對齊 import 重對齊
 from SuiSiannAdminApp.admins.分句聽拍欄位 import 分句欄位
 from SuiSiannAdminApp.views.diffView import DiffView
 
