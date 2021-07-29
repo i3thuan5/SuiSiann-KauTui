423d2996326a7313d218dbddd7add1080a212d80
Fafofafoy <ihcaoe@gmail.com>
2021-07-29 14:45:09 +0800

    相關函式嘛毋免--ah

--- phiaua/static/phiaua/js/lomaji.js
+++ phiaua/static/phiaua/js/lomaji.js
@@ -11,11 +11,9 @@ document.addEventListener('DOMContentLoaded', function(){
     let luimia = lui => lui.miâ;
     let luisik = lui => lui.siktsuí;
     let liuamia = lui => 'liua-' + lui.id;
-    let tueliuamia = lui => 'tueliua-' + lui.id;
     let cssmia = lui => 'lui-' + lui.id;
     let sik = lui => 'sik-' + lui.id;
     let liuatsua = lui_kiatko.map(lui => liuamia(lui)).join(' ')
-    let tueliuatsua = lui_kiatko.map(lui => tueliuamia(lui)).join(' ')
     let thautsing_html = function (node) {
       if(node){
         if(node.outerHTML) //span, p
@@ -30,34 +28,16 @@ document.addEventListener('DOMContentLoaded', function(){
       selector: 'textarea.phiaua',
 
       menubar: false,  
-      toolbar: 'tiongng-siann | '  + liuatsua + ' | tiongng-siann ',
+      toolbar: 'tiongng-siann | '  + liuatsua,
       valid_elements: 'p,span[class]',
       valid_classes: lui_kiatko.map(lui => cssmia(lui)).join(' '),
       valid_styles: {'*': ''},
       setup: function (editor) {
-        editor.ui.registry.addContextToolbar('textselection', {
-          predicate: function (node) {
-            return !editor.selection.isCollapsed();
-          },
-          items: 'tiongng-siann | '  + tueliuatsua + ' | tiongng-siann ',
-          position: 'selection',
-          scope: 'node'
-        });
-        editor.ui.registry.addContextToolbar('text_tshih', {
-          predicate: function (node) {
-            return editor.selection.isCollapsed();
-          },
-          items: 'tiongng-siann',
-          position: 'selection',
-          scope: 'node'
-        });
-
         lui_kiatko.forEach(lui => {
           editor.ui.registry.addIcon(
             cssmia(lui),
             `<svg width="24" height="24" style="fill: ${luisik(lui)};"><path d="M17.5 11.4A9 9 0 0118 14c0 .5 0 1-.2 1.4 0 .4-.3.9-.5 1.3a6.2 6.2 0 01-3.7 3 5.7 5.7 0 01-3.2 0A5.9 5.9 0 017.6 18a6.2 6.2 0 01-1.4-2.6 6.7 6.7 0 010-2.8c0-.4.1-.9.3-1.3a13.6 13.6 0 012.3-4A20 20 0 0112 4a26.4 26.4 0 013.2 3.4 18.2 18.2 0 012.3 4zm-2 4.5c.4-.7.5-1.4.5-2a7.3 7.3 0 00-1-3.2c.2.6.2 1.2.2 1.9a4.5 4.5 0 01-1.3 3 5.3 5.3 0 01-2.3 1.5 4.9 4.9 0 01-2 .1 4.3 4.3 0 002.4.8 4 4 0 002-.6 4 4 0 001.5-1.5z" fill-rule="evenodd"></path></svg>`
           );
-          let tsitma = false;
           editor.ui.registry.addToggleButton(liuamia(lui), {
             icon: cssmia(lui),
             text: luimia(lui),
@@ -80,19 +60,7 @@ document.addEventListener('DOMContentLoaded', function(){
             onSetup: function (api) {
               editor.formatter.formatChanged(sik(lui), function (state) {
                 api.setActive(state);
-                tsitma = state;
               });
-            }
-          });
-          editor.ui.registry.addToggleButton(tueliuamia(lui), {
-            icon: cssmia(lui),
-            text: luimia(lui),
-            onAction: function (_) {
-              editor.execCommand('RemoveFormat');
-              editor.execCommand('FormatBlock', false, sik(lui));
-            },
-            onSetup: function (api) {
-              api.setActive(tsitma);
             }
           });
         });
