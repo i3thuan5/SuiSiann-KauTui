a08873babbf8423ded731ee6d0be90a3c77950d0
Sih Sîng-hông薛丞宏 <ihcaoe@gmail.com>
2021-08-03 09:38:32 +0800

    Update settings.py

--- SuiSiannAdmin/settings.py
+++ SuiSiannAdmin/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Siat-tīng tsò 14.5 kang, khah bē tsò-tsò-leh ting-tshut.
+# Siat-tīng tsò 14.5 kang, khah bē tsò-tsò--leh ting-tshut.
 SESSION_COOKIE_AGE = 1252800
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'wavs')
