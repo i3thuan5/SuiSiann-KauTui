75b2f414bb7d6ea9ecf857139049c9717618b6ae
a8568730 <a8568730@hotmail.com>
2021-08-04 15:57:43 +0800

    Ka feature

---
+++ phiaua/features/steps/hue.py
@@ -0,0 +1,10 @@
+from behave import given, then
+
+@given('口語調類型')
+def 口語調類型(context):
+    raise NotImplementedError(u'STEP: Given 類型')
+
+
+@then('口語調 {html} ê 格式是 {tacotron}')
+def tacotron格式(context):
+    raise NotImplementedError(u'STEP: Given 類型')
