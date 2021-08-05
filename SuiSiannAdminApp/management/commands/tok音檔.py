7cbc8b101e70ab12460901ac54baaeb554b3ba96
a8568730 <a8568730@hotmail.com>
2021-08-05 13:40:12 +0800

    tsiàu kaldi看有欲調整--無。

--- SuiSiannAdminApp/management/commands/tok音檔.py
+++ SuiSiannAdminApp/management/commands/tok音檔.py
@@ -1,3 +1,4 @@
+from itertools import zip_longest
 from django.core.management.base import BaseCommand
 from SuiSiannAdminApp.models import 句表
 import librosa
@@ -18,31 +19,45 @@ class Command(BaseCommand):
         max_gap_duration = options['max_gap_duration']
         gap_thè = max_gap_duration * 0.2
         longtsong = 0
+        besaih = set()
         for 句 in 句表.objects.all():
             if len(句.kaldi切音時間) <= 1:
                 continue
-            wav, sample_rate = librosa.load(句.音檔檔案, sr=None)
-            parts = librosa.effects.split(
-                wav, top_db=options['threshold_db'],
-                frame_length=2048, hop_length=512,
-            )
-            siunn_làng = False
-            tsitma = 0
+            usiann = []
+            print(句.id)
+            for thau, bue in 句.kaldi切音時間:
+                wav, sample_rate = librosa.load(
+                    句.音檔檔案, sr=None,
+                    offset=thau, duration=bue - thau
+                )
+                parts = librosa.effects.split(
+                    wav, top_db=options['threshold_db'],
+                    frame_length=2048, hop_length=512,
+                )
+                # print('parts', parts)
+                usiann.append((
+                    thau + parts[0][0] / sample_rate,
+                    thau + parts[-1][1] / sample_rate
+                ))
             tok = []
-            for tsing, au in zip(parts[:-1], parts[1:]):
-                if (au[0] - tsing[1]) / sample_rate > max_gap_duration:
-                    siunn_làng = True
-                    tok.append((
-                        tsitma,
-                        au[0] / sample_rate - gap_thè
-                    ))
-                    tsitma = tsing[1] / sample_rate + gap_thè
-            if siunn_làng:
-                tok.append((tsitma, len(wav) / sample_rate))
-                longtsong += 1
-                print(句.id, sample_rate)
-                # print(句.kaldi切音時間)
-                # print(parts)
-                print(tok)
-                # return
-        print(longtsong)
+            for tsing, kati, au in zip_longest(
+                [None] + usiann[:-1], usiann, usiann[1:]
+            ):
+                if tsing:
+                    thau = tsing[1] + gap_thè
+                else:
+                    thau = kati[0]
+                if au:
+                    bue = au[0] - gap_thè
+                else:
+                    bue = kati[1]
+                if thau > kati[0] or bue < kati[1]:
+                    besaih.add(句.id)
+                    continue
+                    # print(句.kaldi切音時間)
+                    # print(usiann)
+                    # raise RuntimeError('!!')
+                tok.append((thau, bue))
+            longtsong += 1
+            print(tok)
+        print(longtsong, len(besaih))
