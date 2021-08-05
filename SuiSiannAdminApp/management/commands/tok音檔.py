fa0c3f6451245d4c6c975fbf8ac944cc0375891d
a8568730 <a8568730@hotmail.com>
2021-08-05 11:22:11 +0800

    揣著6音檔siunn長。

--- SuiSiannAdminApp/management/commands/tok音檔.py
+++ SuiSiannAdminApp/management/commands/tok音檔.py
@@ -12,21 +12,25 @@ class Command(BaseCommand):
         parser.add_argument('--min_duration', type=float, default=1.0, help='In seconds')
         parser.add_argument('--max_duration', type=float, default=10.0, help='In seconds')
         parser.add_argument('--max_gap_duration', type=float, default=0.75, help='In seconds')
-        parser.add_argument('--threshold', type=float, default=40.0, help='In decibels below max')
+        parser.add_argument('--threshold_db', type=float, default=40.0, help='In decibels below max')
 
     def handle(self, *args, **options):
+        longtsong = 0
         for 句 in 句表.objects.all():
             if len(句.kaldi切音時間) <= 1:
                 continue
-            print(句.id, 句.kaldi切音時間)
-            wav, sample_rate = librosa.load(句.音檔所在)
+            wav, sample_rate = librosa.load(句.音檔檔案, sr=None)
             parts = librosa.effects.split(
                 wav, top_db=options['threshold_db'],
                 frame_length=1024, hop_length=256,
             )
             siunn_làng = False
             for tsing, au in zip(parts[:-1], parts[1:]):
-                if au[0] - tsing[1] > options['max_gap_duration']:
+                if (au[0] - tsing[1]) / sample_rate > options['max_gap_duration']:
                     siunn_làng = True
             if siunn_làng:
-                print(parts)
+                longtsong += 1
+                print(句.id, 句.kaldi切音時間, sample_rate)
+                # print(parts)
+                # return
+        print(longtsong)
