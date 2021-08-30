f5881ed5f86ac44dd3900572d3c4e67ca6398ee9
Fafofafoy <ihcaoe@gmail.com>
2021-08-30 10:51:33 +0800

    補資料庫。

--- kupan/admin.py
+++ kupan/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from SuiSiannAdminApp.admins.句後台 import 句後台
+from kupan.models import Le, Khuán
+from SuiSiannAdminApp.admins.對齊thai仔 import 對齊thai仔
+
+
+@admin.register(Le)
+class LekuAdmin(句後台):
+    list_filter = ['tó一款句辦', 對齊thai仔, ]
+
+    save_on_top = True
+    readonly_fields = (
+        '對齊狀態',
+        '海湧',
+        '修改時間',
+    )
+    fields = (
+        '漢字', '羅馬字含口語調',
+        '海湧',
+        '對齊狀態',
+        'tó一款句辦', '備註',
+        '修改時間',
+    )
+    autocomplete_fields = ['tó一款句辦']
+
+
+@admin.register(Khuán)
+class KhuánAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'miâ']
--- kupan/models.py
+++ kupan/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phiaua.clean import clean_html, get_lomaji
+from SuiSiannAdminApp.management.檢查對齊狀態 import 檢查對齊狀態
 
-# Create your models here.
+
+class Le(models.Model):
+    音檔 = models.FileField(editable=False)
+    漢字 = models.TextField()
+    羅馬字含口語調 = models.TextField()
+    羅馬字 = models.TextField(editable=False)
+    修改時間 = models.DateTimeField(auto_now=True)
+    修改人 = models.ForeignKey(
+        User, editable=False, null=True,
+        on_delete=models.PROTECT,
+    )
+    對齊狀態 = models.CharField(blank=True, max_length=200, default="-")
+    備註 = models.TextField(blank=True)
+    tó一款句辦 = models.ManyToManyField('Khuán', blank=True)
+
+    def clean(self):
+        sin_html = clean_html(self.羅馬字含口語調)
+        self.羅馬字含口語調 = str(sin_html)
+        self.羅馬字 = get_lomaji(sin_html)
+        self.對齊狀態 = 檢查對齊狀態(self.漢字, self.羅馬字, sin_html)
+
+    def __str__(self):
+        return '{} {}'.format(self.pk, self.漢字)
+
+    @property
+    def 音檔檔案(self):
+        return self.音檔.path
+
+    @property
+    def 音檔網址(self):
+        return self.音檔.url
+
+    class Meta:
+        verbose_name = "Le"
+        verbose_name_plural = verbose_name
+
+
+class Khuán(models.Model):
+    miâ = models.CharField(unique=True, max_length=50)
