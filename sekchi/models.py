8fcc4d424b78fed33937b78d80c433c85ca0821b
niauah <khengiong@ithuan.tw>
2022-07-06 11:12:56 +0800

    Add models

--- sekchi/models.py
+++ sekchi/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Sekchi(models.Model):
+    音檔 = models.FileField(blank=True)
+    漢字 = models.TextField()
+    羅馬字含口語調 = models.TextField()
+    羅馬字 = models.TextField(editable=False)
+    part = models.CharField(max_length=5)
+    編號 = models.CharField(max_length=10)
+    修改時間 = models.DateTimeField(auto_now=True)
+    修改人 = models.ForeignKey(
+        User, editable=False, null=True,
+        on_delete=models.PROTECT,
+    )
+
+    class Meta:
+        verbose_name = "汐止腔語料"
+        verbose_name_plural = verbose_name
