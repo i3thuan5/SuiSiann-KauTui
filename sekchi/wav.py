13bfc0f1aaed8ea8dbe6f2567d62395e59f90283
niauah <khengiong@ithuan.tw>
2022-07-07 15:45:03 +0800

    Add regex matching

--- sekchi/wav.py
+++ sekchi/wav.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 import sys
+import re
 
 
 def mikilim_to_wav(path):
     for file in Path(path).glob('**/*'):
-        print(file)
+        matched = re.match(r'[Pp]art([0-9])-\(([0-9]{1,4})\)\.wav', file.name)
+        if not matched:
+            print(file.name)
 
 
 if __name__ == '__main__':
