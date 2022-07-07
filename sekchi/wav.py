7971a09933ad0144e00f7c3b2979c8aa3a74c2f8
niauah <khengiong@ithuan.tw>
2022-07-07 15:22:57 +0800

    `sys.argv`

--- sekchi/wav.py
+++ sekchi/wav.py
@@ -1,10 +1,12 @@
 from pathlib import Path
+import sys
 
 
-def mikilim_to_wav():
-    for file in Path('giliau').glob('**/*'):
+def mikilim_to_wav(path):
+    for file in Path(path).glob('**/*'):
         print(file)
 
 
 if __name__ == '__main__':
-    mikilim_to_wav()
+    path = sys.argv[1]
+    mikilim_to_wav(path)
