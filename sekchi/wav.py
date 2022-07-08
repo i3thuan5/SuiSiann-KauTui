535c1d19fdeeea5a66b0bf42d7f0d4e028b671f4
niauah <khengiong@ithuan.tw>
2022-07-08 10:23:52 +0800

    Put into a list and sort

--- sekchi/wav.py
+++ sekchi/wav.py
@@ -3,14 +3,22 @@ import sys
 import re
 
 
+def element_1_2(element):
+    return (element[1], int(element[2]))
+
+
 def mikilim_to_wav(path):
+    nikiliman = []
     for file in Path(path).glob('**/*'):
         matched = re.match(r'[Pp]art([0-9])-\(([0-9]{1,4})\)\.wav', file.name)
         if not matched:
             print(file.name)
             continue
-        print(file.name)
-        print(matched.group(1, 2))
+        part, number = matched.group(1, 2)
+        nikiliman.append((file, part, number))
+
+    for i in sorted(nikiliman, key=element_1_2):
+        print(i)
 
 
 if __name__ == '__main__':
