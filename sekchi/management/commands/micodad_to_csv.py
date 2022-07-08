015c880fcf446122cb71dd625c6660494739057f
niauah <khengiong@ithuan.tw>
2022-07-08 16:47:35 +0800

    Create dict for 4 parts

--- sekchi/management/commands/micodad_to_csv.py
+++ sekchi/management/commands/micodad_to_csv.py
@@ -3,16 +3,31 @@ from sekchi.models import Sekchi
 from pathlib import Path
 import csv
 
-def micodad():
-    with open('sekchi/csv/part1.csv', mode='r') as f:
+def micodad(path):
+    with open(path, mode='r') as f:
         reader = csv.DictReader(f)
 
-        part1_dict = {}
+        part_dict = {}
         for row in reader:
-            part1_dict[row['編號']] = row['漢字']
-        print(part1_dict)
+            part_dict[row['編號']] = row['漢字']
+        return part_dict
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        micodad()
+        part1_dict = micodad('sekchi/csv/part1.csv')
+        part2_dict = micodad('sekchi/csv/part2.csv')
+        part3_dict = micodad('sekchi/csv/part3.csv')
+        part4_dict = micodad('sekchi/csv/part4.csv')
+
+        which_part = {
+            '1': part1_dict,
+            '2': part2_dict,
+            '3': part3_dict,
+            '4': part4_dict,
+        }
+
+        for i in Sekchi.objects.all():
+            print(i.part, i.編號)
+            i.漢字 = which_part[i.part][i.編號]
+            print(i.漢字)
