e58e5848a6d2c34d110f09f40b699e76a3540352
niauah <khengiong@ithuan.tw>
2022-07-13 14:50:49 +0800

    list_display

--- sekchi/admin.py
+++ sekchi/admin.py
@@ -5,4 +5,4 @@ from sekchi.models import Sekchi
 # Register your models here.
 @admin.register(Sekchi)
 class SekchiAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('漢字', 'part', '編號', '修改時間')
