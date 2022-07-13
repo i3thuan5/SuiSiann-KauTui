8e8f4b03ad361307dc15ae3f5f4e1e157a342a39
niauah <khengiong@ithuan.tw>
2022-07-13 15:18:10 +0800

    Add `<audio>` tag

--- sekchi/admin.py
+++ sekchi/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from sekchi.models import Sekchi
+from django.utils.html import format_html
 
 
 # Register your models here.
 @admin.register(Sekchi)
 class SekchiAdmin(admin.ModelAdmin):
     list_display = ('漢字', 'part', '編號', '修改時間')
-    fields = ('音檔', '漢字', '羅馬字含口語調', 'part', '編號', '修改時間')
-    readonly_fields = ('part', '編號', '修改時間')
+    fields = ('音檔檔案', '漢字', '羅馬字含口語調', 'part', '編號', '修改時間')
+    readonly_fields = ('音檔檔案', 'part', '編號', '修改時間')
+
+    def 音檔檔案(self, obj):
+        音檔html = '''<div>
+                <audio controls><source src='{}'>
+                Your browser does not support the audio element.</audio>
+                </div>
+                '''
+        return format_html(音檔html, obj.音檔.url)
