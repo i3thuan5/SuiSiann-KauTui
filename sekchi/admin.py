8c08f4f6214c7f17735736367615db828995a83c
niauah <khengiong@ithuan.tw>
2022-07-13 16:43:19 +0800

    Media

--- sekchi/admin.py
+++ sekchi/admin.py
@@ -4,6 +4,7 @@ from sekchi.models import Sekchi
 from django.utils.html import format_html
 from tuitse import kiamtsa
 from tuitse.html import tuitse_html
+from phiaua.admin.options import PhiauAModelAdmin
 
 
 class SekchiForm(forms.ModelForm):
@@ -16,7 +17,7 @@ class SekchiForm(forms.ModelForm):
 
 # Register your models here.
 @admin.register(Sekchi)
-class SekchiAdmin(admin.ModelAdmin):
+class SekchiAdmin(PhiauAModelAdmin):
     form = SekchiForm
     list_display = ('漢字', 'part', '編號', '修改時間')
     fields = ('音檔檔案', '漢字', '羅馬字含口語調', '對齊', 'part', '編號', '修改時間')
@@ -32,13 +33,3 @@ class SekchiAdmin(admin.ModelAdmin):
 
     def 對齊(self, obj):
         return tuitse_html(kiamtsa(obj.漢字, obj.羅馬字含口語調))
-
-    class Media:
-        css = {
-            'all': ('sekchi/css/tuitse.css',)
-        }
-        js = (
-            'https://cdn.tiny.cloud/1/7r771z07171zzo2b460fzfdmi25680770i1u6nf3mz6uh1fs/tinymce/5/tinymce.min.js',
-            'phiaua/js/lomaji.js',
-            'phiaua/js/suan_lomaji.js',
-        )
