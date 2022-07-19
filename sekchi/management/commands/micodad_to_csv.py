433c0c23b2121a6f53d89b179dc1c7067ea86c74
Omah <omah@ithuan.tw>
2022-07-19 15:53:14 +0800

    misalof to for no handle

--- sekchi/management/commands/micodad_to_csv.py
+++ sekchi/management/commands/micodad_to_csv.py
@@ -24,10 +24,8 @@ class Command(BaseCommand):
         which_part = {}
         for n in range(1, 5):
             which_part[str(n)] = micodad(f'sekchi/csv/part{n}.csv')
-
-        for i in Sekchi.objects.all():
-            if i.漢字 != '':
-                continue
+            
+        for i in Sekchi.objects.filter(漢字=''):
             i.漢字 = which_part[i.part][i.編號][0]
             i.來源 = which_part[i.part][i.編號][1]
             i.save()
