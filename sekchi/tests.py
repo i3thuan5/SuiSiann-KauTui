9252639fa0a3c49a358266fff7d1254961ffd730
Fafofafoy <hong@ithuan.tw>
2022-07-19 13:50:07 +0800

    無效ê試驗。

--- sekchi/tests.py
+++ sekchi/tests.py
@@ -3,34 +3,7 @@ from django.test.testcases import TestCase
 from sekchi.models import Sekchi
 
 
-# class 匯入csv試驗(TestCase):
-#     def test_有匯漢字(self):
-#         Sekchi.objects.create(
-#             音檔所在='音檔檔名.wav',
-#             part=1,
-#             編號=1,
-#         )
-#         call_command('micodad_to_csv')
-#         self.assertEqual(
-#             Sekchi.objects.get().漢字,
-#             '多元文化欲受人肯定， 語言佇公領域的呈現是真重要的開始。'
-#         )
-
-#     def test_有漢字就mài改(self):
-#         Sekchi.objects.create(
-#             音檔所在='音檔檔名.wav',
-#             part=self.part,
-#             編號=self.pianho,
-#             漢字='有校對--ah',
-#         )
-#         call_command('micodad_to_csv')
-#         self.assertEqual(Sekchi.objects.get().漢字, '有校對--ah')
-
-
 class 匯入csv來源試驗(TestCase):
-    def tearDown(self):
-        call_command('micodad_to_csv')
-
     def test_來源有寫ê(self):
         Sekchi.objects.create(
             音檔所在='音檔檔名.wav',
