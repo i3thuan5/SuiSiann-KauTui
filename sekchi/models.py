3018043c70402306152df7b1715e64df91cb820d
Fafofafoy <hong@ithuan.tw>
2022-07-19 10:30:07 +0800

    補來源試驗。

--- sekchi/models.py
+++ sekchi/models.py
@@ -27,9 +27,9 @@ class Sekchi(models.Model):
     漢字 = models.TextField()
     羅馬字含口語調 = models.TextField()
     羅馬字 = models.TextField(editable=False)
-    來源 = models.CharField(index=True, max_length=50)
-    part = models.CharField(index=True, max_length=5)
-    編號 = models.CharField(index=True, max_length=10)
+    來源 = models.CharField(max_length=50, db_index=True)
+    part = models.CharField(max_length=5, db_index=True)
+    編號 = models.CharField(max_length=10, db_index=True)
     修改時間 = models.DateTimeField(auto_now=True)
     修改人 = models.ForeignKey(
         User, editable=False, null=True,
--- sekchi/tests.py
+++ sekchi/tests.py
@@ -3,22 +3,66 @@ from django.test.testcases import TestCase
 from sekchi.models import Sekchi
 
 
-class 匯入csv試驗(TestCase):
+# class 匯入csv試驗(TestCase):
+#     def test_有匯漢字(self):
+#         Sekchi.objects.create(
+#             音檔所在='音檔檔名.wav',
+#             part=1,
+#             編號=1,
+#         )
+#         call_command('micodad_to_csv')
+#         self.assertEqual(
+#             Sekchi.objects.get().漢字,
+#             '多元文化欲受人肯定， 語言佇公領域的呈現是真重要的開始。'
+#         )
+
+#     def test_有漢字就mài改(self):
+#         Sekchi.objects.create(
+#             音檔所在='音檔檔名.wav',
+#             part=self.part,
+#             編號=self.pianho,
+#             漢字='有校對--ah',
+#         )
+#         call_command('micodad_to_csv')
+#         self.assertEqual(Sekchi.objects.get().漢字, '有校對--ah')
+
+
+class 匯入csv來源試驗(TestCase):
     def tearDown(self):
-        Sekchi.objects.create(
-            音檔所在='音檔檔名.wav',
-            part='4',
-            編號='2923',
-        )
         call_command('micodad_to_csv')
-        self.assertEqual(Sekchi.objects.get().來源, self.來源)
 
     def test_來源有寫ê(self):
-        self.part = '4'
-        self.pianho = '2923'
-        self.來源 = '三連音'
+        Sekchi.objects.create(
+            音檔所在='音檔檔名.wav',
+            part=4,
+            編號=2924,
+        )
+        call_command('micodad_to_csv')
+        self.assertEqual(Sekchi.objects.get().來源, '三連音')
 
     def test_來源ài看進前ê(self):
-        self.part = '3'
-        self.pianho = 2284
-        self.來源 = '針,tserm(文)'
+        Sekchi.objects.create(
+            音檔所在='音檔檔名.wav',
+            part=3,
+            編號=2315,
+        )
+        call_command('micodad_to_csv')
+        self.assertEqual(Sekchi.objects.get().來源, '鍼,tserm(文)')
+
+    def test_編號kangkhuan3(self):
+        Sekchi.objects.create(
+            音檔所在='音檔檔名.wav',
+            part=3,
+            編號=2258,
+        )
+        call_command('micodad_to_csv')
+        self.assertEqual(Sekchi.objects.get().來源, '連續動詞')
+
+    def test_編號kangkhuan2(self):
+        Sekchi.objects.create(
+            音檔所在='音檔檔名.wav',
+            part=2,
+            編號=2258,
+        )
+        call_command('micodad_to_csv')
+        self.assertEqual(Sekchi.objects.get().來源, '三連音')
