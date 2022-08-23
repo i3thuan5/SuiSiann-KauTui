edef9f0fc5883ca84ecd9030fee62ba1279a81e5
niauah <khengiong@ithuan.tw>
2022-08-23 11:55:41 +0800

    steps kai iong 句表 object

--- phiaua/features/steps/khaugitiau.steps.py
+++ phiaua/features/steps/khaugitiau.steps.py
@@ -1,43 +1,48 @@
 from behave import given, when, then
-from phiaua.clean import clean_html, get_lomaji
-from SuiSiannAdminApp.management.檢查對齊狀態 import 檢查對齊狀態
+# from phiaua.clean import clean_html, get_lomaji
+# from SuiSiannAdminApp.management.檢查對齊狀態 import 檢查對齊狀態
+from SuiSiannAdminApp.models import 文章表, 句表
 
 
 @given(u'有一句錄音')
 def 有一句錄音(context):
-    pass
+    bun = 文章表.objects.create(文章名='試驗用')
+    context.ku = 句表.objects.create(來源=bun)
 
 
 @when(u'輸入口語調 {html}')
 def 輸入(context, html):
-    context.sin_html = clean_html(html)
+    context.ku.羅馬字含口語調 = html
+    context.ku.clean()
+    context.ku.save()
 
 
 @then(u'口語調khiām做 {kiatko}')
 def khiām做(context, kiatko):
-    context.test.assertEqual(str(context.sin_html), kiatko)
+    context.test.assertEqual(context.ku.羅馬字含口語調, kiatko)
 
 
 @when(u'漢字是 {hanji} ，口語調是 {khaugitiau}')
 def 漢字_口語調(context, hanji, khaugitiau):
-    sin_html = clean_html(khaugitiau)
-    lomaji = sin_html.get_text()
-    context.tuitse = 檢查對齊狀態(hanji, lomaji, sin_html)
+    context.ku.漢字 = hanji
+    context.ku.羅馬字含口語調 = khaugitiau
+    context.ku.clean()
+    context.ku.save()
 
 
 @then(u'顯示錯誤 {tshogoo}')
 def 顯示錯誤(context, tshogoo):
-    context.test.assertEqual(context.tuitse, tshogoo)
+    context.test.assertEqual(context.ku.對齊狀態, tshogoo)
 
 
 @then(u'無顯示錯誤')
 def 無顯示錯誤(context):
-    context.test.assertEqual(context.tuitse, '')
+    context.test.assertEqual(context.ku.對齊狀態, '', (context.ku.漢字, context.ku.羅馬字))
 
 
 @then(u'純文字羅馬字khiām做 {lomaji}')
 def step_impl(context, lomaji):
     context.test.assertEqual(
-        get_lomaji(context.sin_html),
+        context.ku.羅馬字,
         lomaji.replace('\\n', '\n'),
     )
