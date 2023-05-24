2a6ace43f262fe3093ab285d65f8515e2f38eb7d
Tira <hong@ithuan.tw>
2023-05-24 08:50:34 +0800

    新資料可能無修改時間。

--- sekchi/models.py
+++ sekchi/models.py
@@ -40,7 +40,7 @@ class Sekchi(models.Model):
     來源 = models.CharField(max_length=50, db_index=True)
     part = models.CharField(max_length=5, db_index=True)
     編號 = models.CharField(max_length=10, db_index=True)
-    修改時間 = models.DateTimeField(editable=False)
+    修改時間 = models.DateTimeField(editable=False, null=True)
     修改人 = models.ForeignKey(
         User, editable=False, null=True,
         on_delete=models.PROTECT,
