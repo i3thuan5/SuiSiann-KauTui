d240b7afc69afa773d57055c94c0a20a8a5a94be
Tira <hong@ithuan.tw>
2023-05-25 16:53:30 +0800

    test毋免音檔--ah。

--- SuiSiannAdmin/settings-citest.py
+++ SuiSiannAdmin/settings-citest.py
@@ -2,8 +2,6 @@ from .settings import *  # noqa
 from .settings import BASE_DIR
 from os.path import join
 
-SUISIANN_ROOT = join(BASE_DIR, 'taiuansuisiann')
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
